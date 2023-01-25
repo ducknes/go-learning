5dbb1343208e172e3d62f183415d27ab00928c34
Ilya Antonov <ilya.anton0v@vk.com>
2023-01-25 15:28:27 +0300

    lesson_6

---
+++ lesson_6/main.go
@@ -0,0 +1,52 @@
+package main
+
+import "fmt"
+
+// Interfaces
+
+type structHere struct {
+	N1, N2 int
+}
+
+func (s *structHere) Sum() int {
+	return s.N1 + s.N2
+}
+
+type InterfaceHere interface {
+	Sum() int
+}
+
+func main() {
+	//var a InterfaceHere
+	// sh := structHere{1, 2}
+	// os := otherStruct{2, 3}
+
+	// a = &sh
+	// fmt.Println(a.Sum())
+	// a = os
+	// fmt.Println(a.Sum())
+
+	// var os *structHere
+	// var i InterfaceHere
+	// i = os
+	// fmt.Println(i.Sum())
+	// fmt.Printf("(%v, %T)\n", i, i)
+	// if i == nil {
+	// 	fmt.Println("nil")
+	// } else {
+	// 	fmt.Println("not nil")
+	// }
+
+	var a interface{} = "hello"
+
+	s := a.(string)
+	fmt.Println(s)
+}
+
+type otherStruct struct {
+	A, B int
+}
+
+func (o otherStruct) Sum() int {
+	return o.A + o.B
+}
---
+++ lesson_6/main2.go
@@ -0,0 +1,33 @@
+package main
+
+var _ User = &user{}
+
+type user struct {
+	FIO, Address, Phone string
+}
+
+type User interface {
+	ChangeFIO(newFio string)
+	ChangeAdress(newAddress string)
+}
+
+func (u *user) ChangeFIO(newFio string) {
+	u.FIO = newFio
+}
+
+func (u *user) ChangeAdress(newAddress string) {
+	u.Address = newAddress
+}
+
+func NewUser(fio, address, phone string) User {
+	u := user{
+		FIO:     fio,
+		Address: address,
+		Phone:   phone,
+	}
+	return &u
+}
+
+func main() {
+
+}
