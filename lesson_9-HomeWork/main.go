1088a489378954b713be11809e26a480d2435181
Ilya Antonov <ilya.anton0v@vk.com>
2023-01-30 14:06:45 +0300

    homework

--- lesson_9-HomeWork/main.go
+++ lesson_9-HomeWork/main.go
@@ -2,18 +2,73 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 	"strings"
 )
 
+// HomeWork: Без примитивов синхронизации (без пакета sync) написать калькулятор на горутинах.
+// 2+2,3+6,7*7,9/3
+// 4, 9, 49, 3
+
 func main() {
-	str := "2 + 2,3 + 6,7 * 7,9 / 3"
+	ch := make(chan int)
+	// go Add(2, 2, ch)
+	// go Add(3, 6, ch)
+	// go Multiple(7, 7, ch)
+	// go Delete(9, 3, ch)
+	strExp := "2+2,3+6,7*7,9/3"
+	go Calc(StrToSlice(strExp), ch)
+	// for i := range ch {
+	// 	fmt.Print(i, " ")
+	// 	count++
+	// 	fmt.Println(count)
+	// }
 
-	s := strings.Split(str, ",")
-	fmt.Println(s)
+	fmt.Print(" ", <-ch)
+	fmt.Print(" ", <-ch)
+	fmt.Print(" ", <-ch)
+	fmt.Print(" ", <-ch)
+}
 
-	var spl [][]string
-	for i := 0; i < len(s); i++ {
-		spl = append(spl, strings.Split(s[i], " "))
+func Add(a, b int, exit chan int) {
+	exit <- a + b
+}
+
+func Take(a, b int, exit chan int) {
+	exit <- a - b
+}
+
+func Multiple(a, b int, exit chan int) {
+	exit <- a * b
+}
+
+func Delete(a, b int, exit chan int) {
+	exit <- a / b
+}
+
+func StrToSlice(s string) []string {
+	return strings.Split(s, ",")
+}
+
+func Calc(expressions []string, exitChan chan int) {
+	for i := range expressions {
+		switch expressions[i][1:2] {
+		case "+":
+			num1, _ := strconv.Atoi(expressions[i][0:1])
+			num2, _ := strconv.Atoi(expressions[i][2:])
+			exitChan <- num1 + num2
+		case "-":
+			num1, _ := strconv.Atoi(expressions[i][0:1])
+			num2, _ := strconv.Atoi(expressions[i][2:])
+			exitChan <- num1 - num2
+		case "*":
+			num1, _ := strconv.Atoi(expressions[i][0:1])
+			num2, _ := strconv.Atoi(expressions[i][2:])
+			exitChan <- num1 * num2
+		case "/":
+			num1, _ := strconv.Atoi(expressions[i][0:1])
+			num2, _ := strconv.Atoi(expressions[i][2:])
+			exitChan <- num1 / num2
+		}
 	}
-	fmt.Println(spl) 
 }
