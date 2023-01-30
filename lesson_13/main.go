2e979ada4b848f15c58a0ce903e590e9c065b4f2
Ilya Antonov <ilya.anton0v@vk.com>
2023-01-30 17:27:48 +0300

    lesson_13

---
+++ lesson_13/main.go
@@ -0,0 +1,37 @@
+package main
+
+// func main() {
+// 	appendToFile()
+// }
+
+// func appendToFile() {
+// 	f, err := os.OpenFile("lesson_13/test.txt", os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0600)
+// 	if err != nil {
+// 		panic(err)
+// 	}
+// 	defer f.Close()
+// 	if _, err = f.WriteString(" Ilya"); err != nil {
+// 		panic(err)
+// 	}
+
+// }
+
+// func writeToFile() {
+// 	data := []byte("my name is Ilya")
+// 	err := os.WriteFile("lesson_13/test.txt", data, 0777)
+// 	if err != nil {
+// 		fmt.Println(err)
+// 	}
+// }
+
+// func readFile() {
+// 	data, err := os.ReadFile("lesson_13/test.txt")
+// 	if err != nil {
+// 		panic(err)
+// 	}
+// 	fmt.Println(string(data))
+// }
+
+func main() {
+	//c := exec.Command("ssh", "-t", "kart", "top")
+}
